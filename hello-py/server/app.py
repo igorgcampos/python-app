10cb2dd1f06455b7ffcfb8bc12001d8bf453ae31
Igor <igor.gonsalves@gmail.com>
2024-03-21 14:47:58 -0300

    teste de commit

--- hello-py/server/app.py
+++ hello-py/server/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "hello, py!"  # Retorne a string diretamente
+    return "hello, py!!!"  # Retorne a string diretamente
 
 if __name__ == "__main__":
     app.run(debug=True)
